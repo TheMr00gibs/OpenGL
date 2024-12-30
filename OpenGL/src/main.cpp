4fa00ed0b299eb60c69c50584193d6bf2792c6bc
TheMr00gibs <racheter.02gilets@icloud.com>
2024-12-30 13:16:55 +0100

    ✨ Use index buffer

--- OpenGL/src/main.cpp
+++ OpenGL/src/main.cpp
@@ -114,10 +114,16 @@ int main(void)
     std::cout << glGetString(GL_VERSION) << std::endl;
 
 
-    float positions[6] = {
-        -0.5f, -0.5f,
-         0.0f,  0.5f,
-         0.5f, -0.5f
+    float positions[] = {
+        -0.5f, -0.5f, // 0
+         0.5f, -0.5f, // 1
+         0.5f,  0.5f, // 2
+        -0.5f,  0.5f, // 3
+    };
+
+    unsigned int indices[] = {
+        0, 1, 2,
+        2, 3, 0
     };
 
     unsigned int buffer;
@@ -133,7 +139,7 @@ int main(void)
     // This means that all subsequent buffer-related operations will affect this bound buffer.
     // `GL_ARRAY_BUFFER` is used to store data such as vertex positions, colors, etc.
 
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
     // Allocate memory for the bound buffer and copy the provided data (`positions`) into it.
     // - The first argument, `GL_ARRAY_BUFFER`, confirms the type of the bound buffer.
     // - The second argument, `6 * sizeof(float)`, specifies the size of the data in bytes (6 floats in this case).
@@ -155,6 +161,12 @@ int main(void)
     // - The fifth argument (`sizeof(float) * 2`) is the stride, or the total size (in bytes) of one vertex's data.
     // - The sixth argument (`0`) is the offset in the buffer where the data for this attribute starts (no offset here).
 
+    // CREATE INDEX BUFFER OBJECT (IBO)
+    unsigned int ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+    
     ShaderProgramSource source = ParseShader("res/shaders/Basic.shader");
     std::cout << "VERTEX" << std::endl;
     std::cout << source.VertexSource << std::endl;
@@ -172,7 +184,7 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3); // Draw call
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr); // Draw call
         
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
